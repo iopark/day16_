9e7e92894ed727de54120fc45b3c86a3ec3c16bb
here_io <52244687+iopark@users.noreply.github.com>
2023-04-14 20:10:46 +0900

    feat: 5번 문제 풀이 구현 기능 추가
    3개의 공통분모는 1,2, 그리고 2,3번의 공통분모의 공통분모를, 유니크한 값을 선별한것과 동일하기에, 최종적으로 Distinct()를 목적을 두고 구현

---
+++ day16_Task/Q5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day16_test_commit
+{
+    internal class Q5
+    {
+        public static void Test5()
+        {
+            static int[] FindCommonInt(int[] ar1, int[] ar2, int[] ar3)
+            {
+                int[] overlap1 = ar1.Intersect(ar2).ToArray();
+                int[] overlap2 = ar2.Intersect(ar3).ToArray();
+                int[] overlapfinale = overlap1.Intersect(overlap2).ToArray();
+
+                return overlapfinale.Distinct().ToArray();
+
+            }
+        }
+        
+
+    }
+}
