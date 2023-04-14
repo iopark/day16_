8586ecdf6fad7cd5054f8c90fb532f31387b0ff8
here_io <52244687+iopark@users.noreply.github.com>
2023-04-14 20:04:42 +0900

    feat: 세번째 문제 풀이 기능 추가
    소수값을찾기위해 Bool 값반환하는 함수를 이용한것은 true/false로 소수를 판별하기 위함이다. 소수는자신이나 1 외에는 나눠지지 않기에 포문을이용하여 값을 확인한다.

---
+++ day16_Task/Q3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day16_test_commit
+{
+    internal class Q3
+    {  
+        // Q3 구현 문제 
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1) return false;
+            for (int i = n - 1; i >= 2; i--)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static void Test3()
+        {
+            string input = Console.ReadLine();
+            int input_conv = int.Parse(input);
+            bool verdict = IsPrime(input_conv);
+            Console.WriteLine($"{verdict}: {input_conv} 은 프라임수가 아닙니다");
+        }
+    }
+}
