d8c8168856913ebc2f6f7cfaea1d1d0a65616304
here_io <52244687+iopark@users.noreply.github.com>
2023-04-14 21:44:43 +0900

    feat: 7번째 질문 답변 구현 위한 맵생성 배열 추가
    유저는 랜덤한 숫자를 규칙에 맞게 정리해야하기에 랜덤한 숫자를 지닌 5x5배열 구현

---
+++ Q7/Map.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    public struct Map
+    {
+        static int[] Generate()
+        {
+            int[] map_deriv = new int[25];
+            //int[] temp = new int[25];
+
+            for (int i = 0; i < 25;)
+            {
+                Random random = new Random();
+                int newGen = random.Next(1, 26);
+
+                //Console.WriteLine(newGen);
+
+                bool check = map_deriv.Contains(newGen);
+
+                if (!check)
+                {
+                    map_deriv[i] = newGen;
+                    i++;
+                }
+                else
+                {
+                    //Console.WriteLine("false");
+                    continue;
+                }
+
+
+            }
+            return map_deriv;
+        }
+
+        static int[,] IntoMatrix(int[] map)
+        {
+            int count = 0;
+            int[,] matrix = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = map[count];
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Q7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Q7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = new int[25];
+            test = Generate(); 
+            foreach (int i in test)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+    }
+}
