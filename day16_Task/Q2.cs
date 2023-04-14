3c9b87e419d14d6aee087da082de7ccfe291abaa
here_io <52244687+iopark@users.noreply.github.com>
2023-04-14 19:54:35 +0900

    feat: 두번째 문제 풀이 추가
    입력값을 string.Replace()한것은 newline키워드를 없애기 위함이고, 이후 스트링의 스플릿메서드를 이용한것은 배열로 저장하기 위함이다. 배열.Length를 이용한것은 찾는결과값과 동일하기 때문이다.

---
+++ day16_Task/Q2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day16_test_commit
+{
+    internal class Q2
+    {
+        public static void Test2()
+        {
+            string input = Console.ReadLine();
+            string trim = input.Replace("\n", "");
+            string[] input_ar = input.Split(" ");
+            Console.WriteLine(input_ar.Length);
+
+        }
+    }
+}
