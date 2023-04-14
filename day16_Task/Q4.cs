27105a7bdbe2fdb27486cf5fb71bddff513262ee
here_io <52244687+iopark@users.noreply.github.com>
2023-04-14 20:08:01 +0900

    feat: 4번 문제 풀이 구현 기능 추가
    입력된값을 무조건적으로 하나의 숫자로 이해하기에, 스플릿을 하여 .Sum을 이용하기전 방해가 될만한 요소를 선 제거 하기위해 장치들을 설정한다.

---
+++ day16_Task/Q4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day16_test_commit
+{
+    internal class Q4
+    {
+        //시험문제 4번 문제 풀이 
+        public void Test4()
+        {
+            Console.Write("입력: ");
+            string input = Console.ReadLine();
+            input = input.Replace(" ", "").Replace("\n", "");
+            char[] input_ar = input.ToCharArray();
+            int[] int_ar = Array.ConvertAll(input_ar, i => int.Parse(i.ToString()));
+            int sum = int_ar.Sum();
+            Console.Write("출력: ");
+            Console.WriteLine(sum);
+        }
+    }
+}
