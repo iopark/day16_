eb69162b8a764d2cba28507bab6017c3dfc4927a
onedupark@outlook.com <onedupark@outlook.com>
2023-04-16 12:11:35 +0900

    feat: 일곱번째 게임 유저인풋받는 기능 추가
    때문에 반환값이 디렉션인것으로 설정한것은, 디렉션값으로 갱신단계에 플레이어와 맵을 디렉션에 따라서 다른 상호작용이 일어나기 때문이다. 또한 해당객체는 콜백형식으로 유저의 인풋을 시점으로 다른객체들을 반응하게 하기에, 이벤트의 predicate형식또한 Direction으로 미리 설정하여둔다.

---
+++ Q7/GameInput.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    public  enum Direction
+    {
+        Up, Down, Left, Right, None
+    }
+    public class GameInput
+    {
+        public Direction direction_; 
+        public Action placedInput; 
+        
+        public Direction direction ()
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                this.direction_ = Direction.Up; break;
+
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S: 
+                this.direction_ = Direction.Down; break;
+
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                this.direction_ = Direction.Left;
+                break;
+
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                this.direction_ = Direction.Left;
+                break;
+
+                default:
+                this.direction_ = Direction.None;
+                    break;
+            }
+            return direction_; 
+        }
+    }
+}
