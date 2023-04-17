bd74accb665a4eb9f7438c6ec5324db934567116
onedupark@outlook.com <onedupark@outlook.com>
2023-04-17 10:02:12 +0900

    feat: 7번째 문제 중 플레이어 구현
    플레이어 객체는 고유의 좌표를 지니게 함으로써 유저입력 이벤트와 함께 갱신객체를 통해서 상호작용이 가능하게 설계함. 추가로 입력, 갱신, 그리고 출력 모두에게 콜링 되야할 객체임으로 원본값을 지닌 클래스로 설정, 그리고 최초 생성당시 항상 콜백대상이 될수있도록 염두에 두고 설계함

--- Q7/Player.cs
+++ Q7/Player.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+// using Numerics.LinearAlgebra; could later implemement library as such (MathNet) in a program that deals more heavily using the matrix values 
 
 namespace Q7
 {
-    public enum Direction
+    
+    public class Player
     {
-        Up, Down, Left, Right, None
-    }
+        private Pointer playerPosition;
+        public Pointer _importPlayerPosition
+        {
+            get { return playerPosition; }
+            set { playerPosition = value; }
+        }
+        private Direction playerDirection;
+        public Direction _importPlayerDirection { get { return playerDirection; } set {  playerDirection = value; } }
 
-    internal class Player
-    {
+        //public Player(Pointer playerPosition, Direction playerDirection)
+        //{
+
+        //}
+        public void Move(Direction direction)
+        {
+            this.playerDirection = direction;
+            //Console.WriteLine($"플레이어가 {direction} 으로 움직였습니다"); 
+        }
+        /// <summary>
+        /// 게임의 시작이벤트와 동시에 호출될 항목으로, 플레이어의 시작점을 설정함 
+        /// </summary>
+        /// <param name="array">5x5 랜덤한 숫자배열 전달바람</param>
+        public void PlayerInitialPosition(int[,] array)
+        {
+            int[] flatarray = array.Cast<int>().ToArray();
+            int find0 = Array.IndexOf(flatarray, 0); 
+            
+            this._importPlayerPosition = new Pointer((find0/array.GetLength(1)), (find0%array.GetLength(1)));
+        }
+        //public void Test1()
+        //{
+        //    Pointer target1 = new Pointer(1,1);
+        //    Pointer target2 = new Pointer(2,2);
+        //    (target1, target2) = target1.SwapPointer(target1, target2);
+
+        //    Console.WriteLine(target1.x);
+        //    Console.WriteLine(target2.x);
+        //}
     }
 }
