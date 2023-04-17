2cbdc74883a79796eeefc2ceaf26487960c7ca75
onedupark@outlook.com <onedupark@outlook.com>
2023-04-17 09:59:46 +0900

    feat: 7번째 문제 맵 객체 구현
    입력에 대해 조건에 충족한다면 갱신을 위해 맵의 맴버변수로써 값갱신을 위한 고유 숫자를 지니며, 추후에 맵 객체가 스스로 입력되는 좌표에 값을 바꾸도록 설정하는데, 단일책임원리에 위배되며, 갱신객체로 옮기는것이 가능하여 보임.

--- Q7/Map.cs
+++ Q7/Map.cs
@@ -1,22 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Q7
 {
-    public struct Map
+    public enum Block
     {
-        static int[] Generate()
+        Numbers, 
+        Wall_TB, 
+        Wall_LR,
+        Wall_Corner
+    }
+    /// <summary>
+    /// 맵을 추상화 하여 나누는것도 가능할것 같다만, 해당하는 효용성에 대한 시험도 추가적으로 필요해 보인다 
+    /// </summary>
+    public class Map
+    {
+        private Block[,] blockMap = new Block[7, 7];
+        public Block[,] blockMap_ {  get { return blockMap; } set { blockMap = value; } }
+        public Pointer number_Pointer { get; set; }
+        private int[,] numberMap; 
+        public int[,] numberMap_ { get { return numberMap; } set { numberMap = value; } }
+        public static int[] Generate_Array()
         {
             int[] map_deriv = new int[25];
             //int[] temp = new int[25];
 
-            for (int i = 0; i < 25;)
+            for (int i = 0; i < 24;)
             {
                 Random random = new Random();
-                int newGen = random.Next(1, 26);
+                int newGen = random.Next(1, 25);
 
                 //Console.WriteLine(newGen);
 
@@ -38,7 +54,7 @@ namespace Q7
             return map_deriv;
         }
 
-        static int[,] IntoMatrix(int[] map)
+        public static int[,] IntoMatrix(int[] map)
         {
             int count = 0;
             int[,] matrix = new int[5, 5];
@@ -47,9 +63,51 @@ namespace Q7
                 for (int j = 0; j < 5; j++)
                 {
                     matrix[i, j] = map[count];
+                    count++;
                 }
             }
             return matrix;
         }
+
+        public void numberSwap(int left_x, int left_y, int right_x, int right_y)
+        {
+            int temp = this.numberMap_[left_x, left_y];
+            this.numberMap_[left_x,left_y] = this.numberMap_[right_x, right_y];
+            this.numberMap_[right_x, right_y] = temp; 
+        }
+
+        public void SetNumberMap ()
+        {
+            this.numberMap = IntoMatrix(Generate_Array()); 
+        }
+
+        public Block[,] SetBlockMap()
+        {
+            for (int y = 0; y < 7; y++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    if ((x, y) == (0, 0) || (x, y) == (6, 0) || (x, y) == (0,6) || (x, y) == (6,6))
+                    {
+                        this.blockMap[x, y] = Block.Wall_Corner;
+                        continue;
+                    }
+                    else if (y == 0 || y == 6)
+                    {
+                        this.blockMap[x, y] = Block.Wall_TB;
+                        continue;
+                    }
+                    else if (x == 0 || x == 6)
+                    {
+                       this.blockMap[x, y] = Block.Wall_LR;
+                       continue;
+                    }
+                }
+            }
+            return this.blockMap; 
+            
+        }
+
+
     }
 }
