982f613aaa607f8c62d33942718eb2674df3ece4
onedupark@outlook.com <onedupark@outlook.com>
2023-04-17 10:05:48 +0900

    feat: 7번째 문제 게임 시스탬 구현
    게임 실행 자체에서 제작된 객체간의 상호작용을 바탕으로 설계해야만 실행이 가능하기에, 아직은 개선의 여지가 보이고, 아직은 솔직히 감이 잘 오지를 않음.

--- Q7/Game.cs
+++ Q7/Game.cs
@@ -1,13 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Q7
 {
-    internal class Game
+    public class Game
     {
-        public static void 
+        GameRender gameRender;
+        GameUpdate gameUpdate;
+        GameInput gameInput;
+        Map map; 
+        Player player; 
+        Direction direction;
+        public bool gameStop = false; 
+        public event Action GameStart; 
+        public event Action GameStop;
+        //public static void 
+
+        public void InIt()
+        {
+            map = new Map();
+            player = new Player();
+            gameInput = new GameInput(player);
+            gameUpdate = new GameUpdate(map, player);
+            gameRender = new GameRender(map, player);
+            direction = new Direction();
+            map.SetNumberMap(); // sets map.numberMap
+            map.SetBlockMap(); // sets map.BlockMap 
+            player.PlayerInitialPosition(map.numberMap_);
+            initialChainEvent();
+
+            Console.WriteLine("시작하시려면 아무키나 입력하세요");
+            Console.ReadKey();
+            //gameRender.RenderMap(); 
+            //gameUpdate.Update();
+            //gameRender.RenderMap();
+            //gameStart += player.
+             while (!gameStop)
+            {
+                //gameUpdate = new GameUpdate(map, player);
+                Loop();
+            }
+            Stop();
+        }
+
+        public void initialChainEvent()
+        {
+            gameInput.userTry += player.Move;
+            gameInput.userTry += gameUpdate.NewPlayerPoint; 
+            gameUpdate.mapUpdate += gameRender.RenderGame;
+            GameStop += gameRender.RenderEnd; 
+        }
+        public void Loop()
+        {
+            
+            gameRender.RenderGame();
+            gameInput.playerInsert();
+            gameUpdate.Update();
+            
+            gameStop = gameUpdate.IsDone(); 
+        }
+
+        public void Stop()
+        {
+            gameInput.userTry -= player.Move;
+            gameInput.userTry -= gameUpdate.NewPlayerPoint;
+            gameUpdate.mapUpdate -= gameRender.RenderGame; 
+            //gameUpdate.mapUpdate -= gameRender.RenderMap;
+            GameStop?.Invoke();
+        }
+
+        public void GameTest()
+        {
+            
+        }
+        
     }
 }
