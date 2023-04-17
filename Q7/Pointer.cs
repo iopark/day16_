8f4179206e889a52af1b4b20bf4c6f7d239387a5
onedupark@outlook.com <onedupark@outlook.com>
2023-04-17 10:04:03 +0900

    feat: 7번째 문제, 포인터 객체 구현
    포인터는 유일하게 맵, 플레이어에서의 고유의값으로써 값자체에 중요성을 띄는 대상임으로 구조체로 설정하고, 다른 객체의 고유맴버가 되더라도 값 복사 및 초기화가 용이하도록 설계 및 구현한다.

---
+++ Q7/Pointer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    /// <summary>
+    /// 값 위주로 진행함으로 stuct형태로 포인터 구현, 
+    /// </summary>
+    public struct Pointer
+    {
+        public int x;
+        public int y;
+        public Pointer(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
+        public (Pointer, Pointer) SwapPointer(Pointer pointer1, Pointer pointer2)
+        {
+            //int temp_x = this.x;
+            //int temp_y = this.y;
+            //pointer1 = new Pointer (pointer2.x, pointer2.y ); 
+            //pointer2 = new Pointer(temp_x, temp_y);
+
+            return (pointer2, pointer1);
+        }
+    }
+}
