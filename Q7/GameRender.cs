10442025b325559f4fa8e97410016246a26d5545
onedupark@outlook.com <onedupark@outlook.com>
2023-04-17 10:08:22 +0900

    feat: 7번째 문제 값 갱신, 그리고 출력 기능
    출력 기능은 갱신객체의 자체 이벤트로 콜백이 되게 되어있는데, 이는 갱신이 이루어 질때만 출력 기능이 구현되기에 이로써 구현함. 비록 프레임 자체가 입력 시에만 구현되기에 차라리 폴링방식이 더 적합하다고 볼수 있겠지만 객체지향적 관점을 향상을 위해 이같이 진행해 보았다.

---
+++ Q7/GameRender.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    /// <summary>
+    /// 게임의 입력, 그리고 갱신이후 출력을 위해, 다음 프레임을 생성하기 위한 캡슐화된 객체 
+    /// </summary>
+    ///
+    public struct RenderPoints
+    {
+        public string blank = "  ";
+        public string sideWall = "|";
+        public string floorCeiling = "-";
+        public string corner = "+";
+        public RenderPoints(string blank, string sideWall, string floorCeiling, string corner) 
+        {
+            this.blank = blank;
+                this.sideWall = sideWall;
+            this.floorCeiling = floorCeiling;
+            this.corner = corner;
+        }
+        
+    }
+    public class GameRender
+    {
+        RenderPoints symbols;
+        Player player; 
+        Map map; 
+        Block[,] b_Map; int[,] i_Map;
+        Pointer playerPointer; 
+        public GameRender(Map map, Player player)
+        {
+            this.map = map;
+            this.player = player;
+            this.b_Map = map.blockMap_;
+            this.i_Map = map.numberMap_;
+            playerPointer = player._importPlayerPosition; 
+
+        }
+
+        public void RenderMap()
+        {
+            StringBuilder aBlock = new StringBuilder();
+            for (int y = 0; y < 7; y++)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int x = 0; x < 7; x++)
+                {
+                    if ((x >= 1 && y >= 1) && (x <= 5 && y <= 5))
+                    {
+                        sb.Append(map.numberMap_[x-1,y-1].ToString().PadLeft(4).PadRight(4));
+                        continue; 
+                    }
+                    switch (b_Map[x, y])
+                    {
+                        case Block.Numbers:
+                            sb.Append("  ".ToString().PadLeft(4).PadRight(4));
+                            break;
+                        case Block.Wall_LR:
+                            sb.Append("|".ToString().PadLeft(4).PadRight(4));
+                            break;
+                        case Block.Wall_Corner:
+                            sb.Append("+".ToString().PadLeft(4).PadRight(4)); 
+                            break;
+                        case Block.Wall_TB:
+                            sb.Append("-".ToString().PadLeft(4).PadRight(4));
+                            break;
+                    }
+                    
+                }
+                sb.Append('\n');
+                aBlock.Append(sb);
+            }
+            Console.WriteLine(aBlock.ToString());
+        }
+
+
+        public void RenderGame()
+        {
+            Console.Clear(); 
+            Console.WriteLine("0을 통해 다른숫자를과 교환하여 보세요"); 
+            RenderMap();
+            Console.Write("위: \u2191 아래: \u2193 왼쪽: \u2190 오른쪽: \u2192 끄기: ESC\n");
+            Console.WriteLine($"{player._importPlayerPosition.x},{player._importPlayerPosition.y}");
+            Console.SetCursorPosition(player._importPlayerPosition.x*4+6, player._importPlayerPosition.y+2);
+            
+        }
+
+        public void RenderEnd()
+        {
+            Console.Clear();
+            Console.WriteLine("Thanks for Playing"); 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Q7/GameUpdate.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    public class GameUpdate
+    {
+        public Action mapUpdate;
+        Map map;
+        Block[,] blockMap; int[,] numberMap; 
+        Player player;
+        Pointer updatePointer;
+        Pointer new_Number;
+        private bool upDated = false; 
+        Direction updateDirection;
+        private bool isWall = false;
+        private bool gameDone = false;
+        public GameUpdate(Map map, Player player)
+        {
+            this.map = map;
+            this.updatePointer = player._importPlayerPosition;
+            this.updateDirection = player._importPlayerDirection;
+            this.blockMap = map.blockMap_;
+            this.numberMap = map.numberMap_;
+            this.player = player;
+            this.updatePointer = new Pointer (player._importPlayerPosition.x,player._importPlayerPosition.y);
+        }
+        /// <summary>
+        /// isWall 은 새로운 포인터의 블록값이 숫자가 아니라면 true, 숫자가 맞다면 false 를 반환합니다 
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="blockMap"></param>
+        /// <returns>isWall</returns>
+        public bool TestWall (Pointer point, Block[,] blockMap)
+        {
+            isWall = (blockMap[point.x+1, point.y+1] == Block.Numbers) ? false : true;
+
+            return (blockMap[point.x + 1, point.y + 1] == Block.Numbers) ? false : true;
+
+        }
+
+        public void NewPlayerPoint(Direction direction)
+        {
+            Pointer new_Point = player._importPlayerPosition;
+            switch (direction)
+            {
+                case Direction.Left:
+                    new_Point.x -= 1;
+                    if (TestWall(new_Point, blockMap))
+                    {
+                        upDated = false; 
+                        break;
+                    }
+                    else
+                        upDated = true;
+                    map.number_Pointer = new Pointer(player._importPlayerPosition.x, player._importPlayerPosition.y);
+                    player._importPlayerPosition = new_Point;
+                        break;
+                case Direction.Right:
+                    new_Point.x += 1;
+                    if (TestWall(new_Point, blockMap))
+                    {
+                        upDated = false;
+                        break;
+                    }
+                    else
+                        upDated = true;
+                    map.number_Pointer = new Pointer(player._importPlayerPosition.x, player._importPlayerPosition.y);
+                        player._importPlayerPosition = new_Point;
+                    break;
+                case Direction.Up:
+                    new_Point.y -= 1;
+                    if (TestWall(new_Point, blockMap))
+                    {
+                        upDated = false;
+                        break;
+                    }
+                    else
+                        upDated = true;
+                        map.number_Pointer = new Pointer(player._importPlayerPosition.x, player._importPlayerPosition.y);
+                        player._importPlayerPosition = new_Point;
+                    break;
+                case Direction.Down:
+                    new_Point.y += 1;
+                    if (TestWall(new_Point, blockMap))
+                    {
+                        upDated = false;
+                        break;
+                    }
+                    else
+                        upDated = true; 
+                        map.number_Pointer = new Pointer(player._importPlayerPosition.x, player._importPlayerPosition.y);
+                        player._importPlayerPosition = new_Point;
+                    break;
+                case Direction.Done:
+                    gameDone = true; 
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void SwapPointer()
+        {
+            if (upDated)
+            {
+                Console.Write("Updating");
+                UpdateNumberMap();
+                return;
+            }
+            else
+                Console.Write("None Changed");
+                return;
+        }
+
+        public void UpdateNumberMap()
+        { 
+            map.numberSwap(player._importPlayerPosition.x , player._importPlayerPosition.y, 
+                map.number_Pointer.x, map.number_Pointer.y);
+        }
+
+        public void Update()
+        {
+            SwapPointer();
+            mapUpdate?.Invoke(); //이것은 GameRender 중 Render자체를 실행시킵니다. 
+        }
+
+        public bool IsDone()
+        {
+            return this.gameDone;
+        }
+    }
+}
