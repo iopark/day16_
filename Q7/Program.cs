8bfb90d990ceb89eed2dfe21a557abfab1bb7857
onedupark@outlook.com <onedupark@outlook.com>
2023-04-17 10:09:06 +0900

    feat: 7번째 문제 완료.
    게임 객체 호출, 다른 추가사항은 없다.

--- Q7/Program.cs
+++ Q7/Program.cs
@@ -4,15 +4,21 @@ namespace Q7
 {
     internal class Program
     {
+        // 해당게임은 이벤트를 통해서 상호작용을 일으키는 객체지향적 프로그램을 제작하기 위해 집중되었습니다 
         static void Main(string[] args)
         {
-            int[] test = new int[25];
-            test = Generate(); 
-            foreach (int i in test)
-            {
-                Console.WriteLine(i);
-            }
+            GameRender gameRender;
+            GameUpdate gameUpdate;
+            GameInput gameInput;
+            Map map;
+            Player player;
+            Direction direction;
+
+            Game game = new Game();
+
+            game.InIt(); 
         }
-        
+
+    
     }
 }
